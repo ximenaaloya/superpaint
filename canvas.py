571b6af46f90a86310cf1fa2cb1be5b19975843f
ximenaaloya <ximenaloya85@gmail.com>
2026-02-05 14:48:50 -0600

    avance

--- canvas.py
+++ canvas.py
@@ -12,3 +12,24 @@ class Canvas(QWidget):
         self.last_point = QPoint()
         self.pen_color = QColor("#fff")
         self.pen_color = 2
+
+    def resizeEvent(self, event):
+        if self.width() > self.image.width() or self.heigth > self.image.heigth():
+            new_width = max(self.width(), self.image.width())
+            max_height = max(self.height(), self.image.height())
+            new_image = QImage(new_width, max_height, QImage.Format.Format_ARGB32)
+            new_image.fill(QColor("#2d2d2d"))
+            with QPainter(new_image) as painter:
+                painter.drawImage(0,0,self.image)
+            self.image = new_image
+        super().resizeEvent(event)
+        self.draw_examples()
+
+    def draw_examples(self):
+        with QPainter(self.image) as painter:
+            painter.setPen(QPen(QColor("#f00"),10,Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
+            painter.drawLine(300,0,300,600)
+            painter.drawLine(0,300,600,300)
+            painter.drawRect(265,265,70,70)
+        self.update()
+
