b29cfaeac941d70be3aaef4940d60ad8745096dd
ximenaaloya <ximenaloya85@gmail.com>
2026-02-06 18:30:39 -0600

    ya pintaaaaa

--- canvas.py
+++ canvas.py
@@ -5,25 +5,29 @@ from PyQt6.QtGui import QColor
 class Canvas(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
+        print(1)
         self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
         self.image = QImage(self.size(), QImage.Format.Format_ARGB32)
-        self.image.fill(QColor("rgb(236, 229, 255)"))
+        self.image.fill(QColor("#ece5ff"))
         self.drawing = False
         self.last_point = QPoint()
         self.pen_color = QColor("#fff")
-        self.pen_color = 2
+        self.pen_width = 2
+    def paintEvent(self, event):
+        with QPainter(self) as painter:
+            painter.drawImage(event.rect(), self.image, event.rect())
 
     def resizeEvent(self, event):
         if self.width() > self.image.width() or self.heigth > self.image.heigth():
             new_width = max(self.width(), self.image.width())
             max_height = max(self.height(), self.image.height())
             new_image = QImage(new_width, max_height, QImage.Format.Format_ARGB32)
-            new_image.fill(QColor("#2d2d2d"))
+            new_image.fill(QColor("#ece5ff"))
             with QPainter(new_image) as painter:
                 painter.drawImage(0,0,self.image)
             self.image = new_image
         super().resizeEvent(event)
-        self.draw_examples()
+        #self.draw_examples()
 
     def draw_examples(self):
         with QPainter(self.image) as painter:
@@ -32,4 +36,22 @@ class Canvas(QWidget):
             painter.drawLine(0,300,600,300)
             painter.drawRect(265,265,70,70)
         self.update()
+    def mousePressEvent(self, a0):
+        if a0.button() == Qt.MouseButton.LeftButton:
+            self.last_point = a0.position().toPoint()#captura de coordenada donde se da click
+            self.drawing = True
+    def mouseMoveEvent(self, a0):
+        if (a0.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
+            self.draw_line_to(a0.position().toPoint())
+    def mouseReleaseEvent(self, a0):
+        if (a0.button() == Qt.MouseButton.LeftButton) and self.drawing:
+            self.draw_line_to(a0.position().toPoint())
+            self.drawing = False
+    def draw_line_to(self, end_point):
+         with QPainter(self.image) as painter:
+            painter.setPen(QPen(self.pen_color,self.pen_width,Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap, Qt.PenJoinStyle.RoundJoin))
+            painter.drawLine(self.last_point, end_point)
+            self.update()
+            self.last_point = end_point
+
 
--- controller.py
+++ controller.py
@@ -3,19 +3,25 @@ class MainController:
     def __init__(self, main_window, ui):
         self.main_window = main_window
         self.ui = ui
+        self.canvas = ui.widget
         self.connect_signals()
 
     #declaramos los eventos
     def connect_signals(self):
         self.ui.txtColor.textChanged.connect(self.update_color)
         self.ui.slider.valueChanged.connect(self.update_pincel)
+        self.ui.btnBorrador.clicked.connect(self.set_eraser)
+    def set_eraser(self):
+        self.canvas.pen_color = QColor("#ece5ff")
     def update_pincel(self, width):
-        print(width)
+        #print(width)
+        self.canvas.pen_width = width
 
     def update_color(self):
         color = self.ui.txtColor.toPlainText().strip()
         print(color)
         fondo = QColor(color)
+        self.canvas.pen_color = fondo
 
         if fondo.isValid():
             self.ui.txtColor.setStyleSheet(f"background-color: {color};color:{self.color_inverse(fondo).name()}")
@@ -23,6 +29,8 @@ class MainController:
     def color_inverse(self, color):
         inverse = QColor(255-color.red(), 255-color.green(), 255-color.blue())
         return inverse
+    
+
     
     
 
